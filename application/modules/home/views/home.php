5d42becdde4ceaa3bb66298b69fc643b6a8182b1
Reymar Guerrero <reymarguerrero@outlook.com>
2015-02-22 20:37:11 +0800

    Added vrymel's changes

---
+++ application/modules/home/views/home.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>TrueLab - Clinic and Diagnostic</title>
+    <link rel="stylesheet" type="text/css" href="//<?php echo site_url('css/bootstrap/bootstrap.min.css');?>">
+</head>
+<body>
+ 
+<div id="container">
+    <div class="col-sm-3 col-md-2 sidebar">
+          <ul class="nav nav-sidebar">
+            <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
+            <li><a href="#">Reports</a></li>
+            <li><a href="#">Analytics</a></li>
+            <li><a href="#">Export</a></li>
+          </ul>
+          <ul class="nav nav-sidebar">
+            <li><a href="">Nav item</a></li>
+            <li><a href="">Nav item again</a></li>
+            <li><a href="">One more nav</a></li>
+            <li><a href="">Another nav item</a></li>
+            <li><a href="">More navigation</a></li>
+          </ul>
+          <ul class="nav nav-sidebar">
+            <li><a href="">Nav item again</a></li>
+            <li><a href="">One more nav</a></li>
+            <li><a href="">Another nav item</a></li>
+          </ul>
+        </div>
+
+</div>
+<script type="text/javascript" src="//<?php echo site_url('js/jquery/jquery-1.11.2.min.js');?>"></script>
+<script type="text/javascript" src="//<?php echo site_url('css/bootstrap/bootstrap.min.js');?>"></script>
+</body>
+</html>
